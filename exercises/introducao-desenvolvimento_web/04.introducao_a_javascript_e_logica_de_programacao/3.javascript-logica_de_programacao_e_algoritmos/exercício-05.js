ab8345812d519f67c60e818e05581246311084fc
Caio Menezes Rocha <caiomzrocha@gmail.com>
2020-10-28 02:58:19 -0300

    Adicionado exercício 05

---
+++ exercises/introducao-desenvolvimento_web/04.introducao_a_javascript_e_logica_de_programacao/3.javascript-logica_de_programacao_e_algoritmos/exercício-05.js
@@ -0,0 +1,17 @@
+let n = 5;
+let mid = Math.floor(n / 2);
+let line = '';
+
+for (let i = 0; i <= mid; i++) {
+	line = '';
+	
+	for (let j = 0; j < n; j++) {
+		if (j === mid - i || j === mid + i || i === mid)
+			line += '*';
+		else
+			line += ' ';
+	}
+
+	console.log(line);
+}
+
