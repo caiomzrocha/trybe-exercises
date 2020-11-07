a50f8cea102a48dfab85beb81b8e7c390c7bd5c9
Caio Menezes Rocha <caiomzrocha@gmail.com>
2020-11-07 12:46:40 -0300

    Adicionado exercício 02 - adição de elementos

--- exercises/introducao-desenvolvimento_web/05.dom-eventos-web_storage/2.javascript-trabalhando_com_elementos/script.js
+++ exercises/introducao-desenvolvimento_web/05.dom-eventos-web_storage/2.javascript-trabalhando_com_elementos/script.js
@@ -1,7 +1,11 @@
 let body = document.querySelector('body');
 let h1 = document.createElement('h1');
+let main_content = document.createElement('div');
+
+main_content.classList.add('main-content');
 
 body.appendChild(h1);
+body.appendChild(main_content);
 
 console.log(body);
 
