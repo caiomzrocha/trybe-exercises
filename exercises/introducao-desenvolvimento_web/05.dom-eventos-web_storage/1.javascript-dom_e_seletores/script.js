a5f3e6450fd0f82285a0e4f403344d8f48c21575
Caio Menezes Rocha <caiomzrocha@gmail.com>
2020-11-07 11:32:26 -0300

    Adicionado exercício 04

--- exercises/introducao-desenvolvimento_web/05.dom-eventos-web_storage/1.javascript-dom_e_seletores/script.js
+++ exercises/introducao-desenvolvimento_web/05.dom-eventos-web_storage/1.javascript-dom_e_seletores/script.js
@@ -3,10 +3,6 @@
    - document.getElementById()
    - document.getElementsByClassName()
    - document.getElementsByTagName()
-   Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício) body > div > div > p - p - p
-   Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)). class ("main-content")
-   Crie uma função que mude a cor do quadrado vermelho para branco. class ("center-content")
-   Crie uma função que corrija o texto da tag <h1>. class ("title")
    Crie uma função que modifique todo o texto da tag <p> para maiúsculo. 
    Crie uma função que exiba o conteúdo de todas as tags <p> no console.
  */
@@ -14,19 +10,25 @@
 let paragraphs = document.getElementsByTagName('p');
 let main_content = document.getElementsByClassName('main-content');
 let center_content = document.getElementsByClassName('center-content');
+let title = document.getElementsByClassName('title');
 
-function changeParagraphText() {
+function setParagraphText() {
   paragraphs[0].innerText = "Em 2022 estarei empregado e recebendo um salário igual ou superior a R$ 3500,00.";
 }
 
-function changeColorMainContent() {
+function setColorMainContent() {
   main_content[0].style.backgroundColor = 'rgb(76,164,109)';
 } 
 
-function changeColorCenterContent() {
+function setColorCenterContent() {
   center_content[0].style.backgroundColor = 'white';
 }
 
-changeParagraphText();
-changeColorMainContent();
-changeColorCenterContent();
+function setTitle() {
+  title[0].innerText = 'Exercício 5.1 - JavaScript';
+}
+
+setParagraphText();
+setColorMainContent();
+setColorCenterContent();
+setTitle();
