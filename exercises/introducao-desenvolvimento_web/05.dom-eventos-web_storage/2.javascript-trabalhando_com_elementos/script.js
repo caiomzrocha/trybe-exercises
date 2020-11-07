473f68832ccd6bf0f69f933710829c920b7257cd
Caio Menezes Rocha <caiomzrocha@gmail.com>
2020-11-07 12:59:06 -0300

    Adicionados exercícios 04 e 05 - adição de elementos

--- exercises/introducao-desenvolvimento_web/05.dom-eventos-web_storage/2.javascript-trabalhando_com_elementos/script.js
+++ exercises/introducao-desenvolvimento_web/05.dom-eventos-web_storage/2.javascript-trabalhando_com_elementos/script.js
@@ -2,13 +2,20 @@ let body = document.querySelector('body');
 let h1 = document.createElement('h1');
 let main_content = document.createElement('div');
 let center_content = document.createElement('div');
+let paragraph = document.createElement('p');
+let left_content = document.createElement('div');
 
 main_content.classList.add('main-content');
 center_content.classList.add('center-content');
+left_content.classList.add('left-content');
 
 body.appendChild(h1);
 body.appendChild(main_content);
 main_content.appendChild(center_content);
+center_content.appendChild(paragraph);
+main_content.appendChild(left_content);
+
+paragraph.innerText = 'Conteúdo central.';
 
 console.log(body);
 
