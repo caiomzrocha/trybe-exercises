ed50c496c030148203b180c124ad5efd1b200ea5
Caio Menezes Rocha <caiomzrocha@gmail.com>
2020-12-03 19:10:02 -0300

    Add exercise-06.js

--- exercises/introducao-desenvolvimento_web/08.high_order_functions/4.spread_operator-rest-destructuring/exercise-05.js
+++ exercises/introducao-desenvolvimento_web/08.high_order_functions/4.spread_operator-rest-destructuring/exercise-05.js
@@ -1,11 +1,11 @@
-const assert = require('assert')
+const assert = require('assert');
 
-const myList = [1, 2, 3]
+const myList = [1, 2, 3];
 
 const swap = ([a, b, c]) => [c, b, a];
 
 const swappedList = swap(myList);
 
-assert.strictEqual(swappedList[0], 3)
-assert.strictEqual(swappedList[1], 2)
-assert.strictEqual(swappedList[2], 1)
+assert.strictEqual(swappedList[0], 3);
+assert.strictEqual(swappedList[1], 2);
+assert.strictEqual(swappedList[2], 1);
